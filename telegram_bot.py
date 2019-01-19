c97d3ed5d9b8fd69c545adb9cd2abd22dd543474
yaniv-l <yanivl@srl.co.il>
2019-01-19 21:43:10 +0200

    telegram commands update

--- telegram_bot.py
+++ telegram_bot.py
@@ -8,17 +8,14 @@ from utils import config
 
 
 
-def get_url(station, image):
-    # TODO - dynamicly scrap the summery image of wind read from page
-    # contents = requests.get('http://wind.co.il/%D7%9E%D7%96%D7%92-%D7%90%D7%95%D7%99%D7%A8/%D7%A9%D7%99%D7%93%D7%95%D7%A8-%D7%97%D7%99/').json()
-    # url = contents['url']
-    config.get(station, image)
-    url = config.get(station, image) + "?t=" + datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
+def get_url(section, image):
+    # Get the url for the specific image command - from on config file
+    url = config.get(section, image) + "?t=" + datetime.datetime.now().strftime("%d/%m/%Y-%H:%M:%S")
     return url
 
 
 def windalert(bot, update):
-    url = get_url("Prigal", "summaryImageURL")
+    url = get_url("botimagecommands", update.message.text.replace("/", ""))
     chat_id = update.message.chat_id
     bot.send_photo(chat_id=chat_id, photo=url)
 
@@ -27,13 +24,14 @@ def sendWindAlert(alert_message):
     updater = Updater(SECRETS.BOTID)
     dp = updater.dispatcher
     dp.bot.send_message(chat_id=SECRETS.CHATID, text=alert_message, parse_mode=ParseMode.MARKDOWN)
-    dp.bot.send_photo(chat_id=SECRETS.CHATID, photo=get_url("Prigal", "summaryImageURL"))
+    dp.bot.send_photo(chat_id=SECRETS.CHATID, photo=get_url("botimagecommands", "prigalsummary"))
 
 
 def init_bot_listener():
     updater = Updater(SECRETS.BOTID)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler('windalert',windalert))
+    commands = config.options("botimagecommands")
+    dp.add_handler(CommandHandler(commands,windalert))
     updater.start_polling()
     updater.idle()
 
